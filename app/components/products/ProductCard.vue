f195f76cbda07417ba598991e73473f5eeb03f56
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2025-12-29 01:23:25 +0330

    feat: product card basic

---
+++ app/components/products/ProductCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product-card-container">
+    <div class="px-4">
+        <div class="fs-14 font-weight-700 dark-blue my-4">درب اتوماتیک بازویی Face مدل SW2</div>
+        <v-btn variant="outlined" class="border-16" color="#E9EDF5" border="sm" block height="40">
+          <span class="fs-14 font-weight-700 dark-pink ml-2">مشاهده جزئیات</span>
+          <v-img src="/icons/chevronLeftIconPink.svg" width="16" height="16" />
+        </v-btn>
+    </div>
+  </div>
+</template>
+<script setup></script>
+
+<style lang="css">
+.product-card-container {
+  border-radius: 24px;
+  border: 1px solid #e9edf5;
+  background-color: #ffffff;
+  width: 263px;
+  height: 286px;
+}
+</style>
