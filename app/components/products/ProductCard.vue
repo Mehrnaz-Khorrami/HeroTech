0ba528ddafd7f22ee953d3d51fb267481c7dfc00
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2026-01-03 16:50:21 +0330

    feat: product card and set dynamic data on it

--- app/components/products/ProductCard.vue
+++ app/components/products/ProductCard.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="product-card-container">
+    <v-img :src="product.image" width="247" height="170" class="ma-2" />
     <div class="px-4">
-        <div class="fs-14 font-weight-700 dark-blue my-4">درب اتوماتیک بازویی Face مدل SW2</div>
-        <v-btn variant="outlined" class="border-16" color="#E9EDF5" border="sm" block height="40">
-          <span class="fs-14 font-weight-700 dark-pink ml-2">مشاهده جزئیات</span>
-          <v-img src="/icons/chevronLeftIconPink.svg" width="16" height="16" />
-        </v-btn>
+      <div class="fs-14 font-weight-700 dark-blue my-4 product-title">
+        {{ product.title }}
+      </div>
+      <v-btn
+        variant="outlined"
+        class="border-16"
+        color="#E9EDF5"
+        border="sm"
+        block
+        height="40"
+        :to="`/products/${product.id}`"
+      >
+        <span class="fs-14 font-weight-700 dark-pink ml-2">مشاهده جزئیات</span>
+        <v-img src="/icons/chevronLeftIconPink.svg" width="16" height="16" />
+      </v-btn>
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+defineProps({
+  product: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
 
 <style lang="css">
 .product-card-container {
@@ -18,5 +36,15 @@
   background-color: #ffffff;
   width: 263px;
   height: 286px;
+}
+
+.product-title {
+  max-height: 24px;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  line-height: 24px;
+  word-wrap: break-word;
 }
 </style>
