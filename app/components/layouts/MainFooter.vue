4a591983f5beb018d65fd9af7955fa5818c3acf7
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2026-01-03 20:22:13 +0330

    feat: main footer styles

--- app/components/layouts/MainFooter.vue
+++ app/components/layouts/MainFooter.vue
@@ -1,3 +1,16 @@
-<template></template>
+<template>
+  <div class="main-footer-container">
+    <FooterLinks />
+  </div>
+</template>
 <script setup></script>
-<style lang="css"></style>
+<style lang="css">
+.main-footer-container {
+  height: 416px;
+  box-shadow: 0px -2px 2px 0px rgba(0, 0, 0, 0.04);
+  width: 1440;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  margin-top: 74px;
+}
+</style>
