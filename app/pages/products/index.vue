d0d38a4857d3cb281ddfcba4a8a8741b8cb3a585
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2026-01-03 16:50:58 +0330

    feat: get products data and pass it as a props to the product card

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -1,7 +1,22 @@
-<template> 
-<div>
-    <!-- test -->
-    <ProductsProductCard/>
-</div>
+<template>
+  <div>
+    <v-row v-if="data && data.length" class="ma-0">
+      <v-col
+        v-for="product in data"
+        :key="product.id"
+        cols="12"
+        sm="6"
+        md="4"
+        class="d-flex justify-center"
+      >
+        <ProductsProductCard :product="product" />
+      </v-col>
+    </v-row>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+// get Products data
+const { data } = await useAsyncData("products", () =>
+  $fetch("https://fakestoreapi.com/products")
+);
+</script>
