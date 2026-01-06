494d8bfe769a6222cfed25bae7dad6a858991f0c
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2026-01-07 02:31:11 +0330

    feat: import products filters and selected filters component

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+ <v-row class="ma-0 mt-6 pa-0" >
+  <!-- Filters (right side in RTL) -->
+  <v-col cols="12" md="3" class="pa-0 pl-4">
+    <div class="filters-wrapper">
+      <ProductsFilters />
+    </div>
+  </v-col>
+
+  <!-- Products (left side in RTL) -->
+  <v-col cols="12" md="9" class="pa-0">
     <v-row v-if="pending" class="ma-0">
       <v-col
         v-for="skeleton in 6"
@@ -12,23 +21,34 @@
         <v-skeleton-loader width="263" type="card" />
       </v-col>
     </v-row>
-    <v-row v-else-if="data && data.length" class="ma-0">
+
+    <v-row v-else-if="data?.length" class="ma-0 pa-0">
+      <v-col cols="12" class="pa-0">
+        <ProductsSelectedFilters/>
+      </v-col>
       <v-col
         v-for="product in data"
         :key="product.id"
         cols="12"
         sm="6"
         md="4"
-        class="d-flex justify-center"
+        class="d-flex justify-center pa-0 mb-6"
       >
         <ProductsProductCard :product="product" />
       </v-col>
     </v-row>
-  </div>
+  </v-col>
+</v-row>
 </template>
+
 <script setup>
-// get Products data
 const { data, pending } = await useAsyncData("products", () =>
   $fetch("https://fakestoreapi.com/products")
 );
 </script>
+<style lang="css" scoped>
+.filters-wrapper {
+  position: sticky;
+  top: 24px;
+}
+</style>
